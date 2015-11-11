9f44a2cce5788dcac0ac27c7e196e51776237098
Maxime Belaïd <MaximeB@MacBook-Pro-de-Maxime.local>
2015-11-11 15:11:16 +0100

    replace theme image

---
+++ src/AvekApeti/HomeBundle/Resources/public/js/home.js
@@ -0,0 +1,10 @@
+function handleHeaderOpacity() {
+    $('.header-opacity').css('height', $('header').css('height'));
+}
+
+$(document).ready(function(){
+    handleHeaderOpacity();
+    $(window).resize(function(){
+        handleHeaderOpacity();
+    });
+});
