d1b7715265058719a0b8ef497ba1c03b15228a83
Lucas <lucasj.james@gmail.com>
2015-12-08 20:14:08 +0100

    Commande d'installations fonctionnelles avekapeti:install

--- src/AvekApeti/BackBundle/Command/Command.php
+++ src/AvekApeti/BackBundle/Command/Command.php
@@ -8,7 +8,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
-use Www\BackBundle\Entity\Groupe;
+use AvekApeti\BackBundle\Entity\Groupe;
+use AvekApeti\BackBundle\Entity\Utilisateur;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -50,7 +51,7 @@ private $commandes = array(
 
        // $this->ensureDirectoryExistsAndIsWritable(self::APP_CACHE, $output);
         $app = $this->getApplication();
-
+        $app->setAutoExit(false);
         foreach ($this->commandes as $step => $command) {
             try {
                 $output->writeln(sprintf('<comment>Step %d of %d.</comment> <info>%s</info>', $step+1, count($this->commandes), $step));
@@ -58,7 +59,6 @@ private $commandes = array(
                 //$app->add();
                // $app->run(new ArrayInput($command),$output);
                 $returnCode = $app->run(new ArrayInput($command),$output);
-                die(var_dump($returnCode));
                 //$output->writeln($output);
             } catch (RuntimeException $exception) {
                 $this->isErrored = true;
@@ -71,25 +71,48 @@ private $commandes = array(
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $groupe = $em->getRepository("AvekApetiBackBundle:Groupe")
             ->findOneByRole('ROLE_GOURMET');
-        if(ISSET($groupe))
+        if(!ISSET($groupe))
         {
+            $output->writeln(sprintf("Creation des droits"));
             $groupe = new Groupe();
+            $groupe1 = new Groupe();
+            $groupe2 = new Groupe();
             $groupe->setName('gourmet');
+            $groupe1->setName('chef');
+            $groupe2->setName('admin');
             $groupe->setRole('ROLE_GOURMET');
+            $groupe1->setRole('ROLE_CHEF');
+            $groupe2->setRole('ROLE_SUPER_ADMIN');
 
             $em->persist($groupe);
+            $em->persist($groupe1);
+            $em->persist($groupe2);
+            $em->flush();
+
+            $output->writeln(sprintf("Creation des admin"));
+
+            $Groupe =$em->getRepository("AvekApetiBackBundle:Groupe")
+                ->findOneByRole('ROLE_SUPER_ADMIN');
+
+            $user = new Utilisateur();
+            $user->setPassword('admin');
+            $user->setEmail('admin@admin');
+            $user->setLogin('Admin');
+            $user->setPhone('Admin');
+            $user->setGroupe($Groupe);
+
+            $factory = $this->getContainer()->get('security.encoder_factory');
+
+            $hash = $factory->getEncoder($user)->encodePassword($user->getPassword(),$user->getSalt());
+
+            $user->setPassword($hash);
+            $em->persist($user);
             $em->flush();
         }
 
-        $map = array(
-            Kernel::ENV_DEV     => '/app_dev.php',
-            Kernel::ENV_TEST    => '/app_test.php',
-            Kernel::ENV_STAGING => '/app_staging.php',
-            Kernel::ENV_PROD    => '/',
-        );
+
 
         $output->writeln($this->getProperFinalMessage());
-        $output->writeln(sprintf('You can now open your store at the following path under the website root: <info>%s.</info>', $map[$this->getEnvironment()]));
     }
 
     /**
